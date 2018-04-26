d98c2be3e6ffc72f6baf1fdabd868302a0362077
Mann <pareshmaniyar1@gmail.com>
2018-04-26 22:42:56 +0530

    Complete Flappy Bird Game

--- src/App.js
+++ src/App.js
@@ -51,8 +51,8 @@ class Game extends React.Component{
         }
         var towers = [
             {position:2,height:7,upright:false},
-            {position:5,height:10,upright:true},
-            {position:8,height:9,upright:false},
+            {position:5,height:8,upright:true},
+            {position:8,height:5,upright:false},
             {position:11,height:7,upright:true},
             {position:14,height:10,upright:false},
             {position:17,height:9,upright:true},
@@ -67,9 +67,11 @@ class Game extends React.Component{
         ]
         grid[bird.height][bird.position] = 'yellow'
 
-        this.state = {grid:grid,bird:bird, towers:towers}
+        this.state = {grid:grid,bird:bird, towers:towers, crashed:false, score:0}
 
         this.timerID = setInterval( () => {
+            if(this.state.crashed)
+                return
             var gridCopy = []
             var towersCopy = this.state.towers.slice()
             for(let i = 0; i < 20; i++){
@@ -92,16 +94,40 @@ class Game extends React.Component{
             }
             var birdCopy = this.state.bird
             birdCopy.height++
+            var crashed = false
             if(birdCopy.height > 19 || birdCopy.height <0){
                 birdCopy.height = 10
+                crashed = true
+            }
+            for(let i = 0; i < 20; i++){
+                if(gridCopy[i][2] == 'blue' && birdCopy.height == i){
+                    birdCopy.height = 10
+                    crashed = true
+                }
+            }
+            if(crashed){
+                this.setState({crashed:true})
             }
             gridCopy[birdCopy.height][birdCopy.position] = 'yellow'
-            this.setState({grid:gridCopy,bird:birdCopy}) 
+            this.setState({grid:gridCopy,bird:birdCopy, towers:towersCopy, score: this.state.score + 1}) 
         },200)        
+    }
+    handleClick(){
+        if(this.state.crashed)
+            return
+        var birdCopy = this.state.bird
+        birdCopy.height-= 3
+        this.setState({bird:birdCopy})
+    }
+    restart(){
+        this.setState({crashed:false, score:0})
     }
     render(){
         return (
+            <div onClick = {this.handleClick.bind(this)}>
             <Grid grid = {this.state.grid}/>
+            {this.state.crashed?<button onClick = {this.restart.bind(this)}>Restart</button>: this.state.score}
+        </div>
             )
     }
 }
